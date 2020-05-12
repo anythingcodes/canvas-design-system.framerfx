00081c53825a3cecb872bebf950eb8d8a98d2e2e
Liz Shaw <liz.h.shaw@gmail.com>
2020-05-12 19:42:02 -0400

    Change default modal type

--- code/generated/Modal.ts
+++ code/generated/Modal.ts
@@ -1,49 +1,49 @@
 // WARNING: This is an auto-generated file. Changes to this file will be lost!
-import { ControlType, PropertyControls, ControlDescription } from "framer"
+import {ControlType, PropertyControls, ControlDescription} from 'framer';
 
 export type Controls = {
-  animate: ControlDescription
-  autofocus: ControlDescription
-  children: ControlDescription
-  closeable: ControlDescription
-  isOpen: ControlDescription
-  role: ControlDescription
-  size: ControlDescription
-}
+  animate: ControlDescription;
+  autofocus: ControlDescription;
+  children: ControlDescription;
+  closeable: ControlDescription;
+  isOpen: ControlDescription;
+  role: ControlDescription;
+  size: ControlDescription;
+};
 
 /**
  * Contains the inferred property controls.
  */
 export const controls: Controls = {
-  animate: { title: "Animate", defaultValue: false, type: ControlType.Boolean },
+  animate: {title: 'Animate', defaultValue: false, type: ControlType.Boolean},
   autofocus: {
-    title: "Autofocus",
+    title: 'Autofocus',
     defaultValue: false,
     type: ControlType.Boolean,
   },
-  children: { title: "Children", defaultValue: "", type: ControlType.String },
+  children: {title: 'Children', defaultValue: '', type: ControlType.String},
   closeable: {
-    title: "Closeable",
+    title: 'Closeable',
     defaultValue: false,
     type: ControlType.Boolean,
   },
-  isOpen: { title: "IsOpen", defaultValue: false, type: ControlType.Boolean },
+  isOpen: {title: 'IsOpen', defaultValue: false, type: ControlType.Boolean},
   role: {
-    title: "Role",
-    options: ["dialog", "alertdialog"],
-    optionTitles: ["Dialog", "Alertdialog"],
-    defaultValue: "dialog",
+    title: 'Role',
+    options: ['dialog', 'alertdialog'],
+    optionTitles: ['Dialog', 'Alertdialog'],
+    defaultValue: 'alertdialog',
     type: ControlType.Enum,
   },
   size: {
-    title: "Size",
-    options: ["auto", "default", "full"],
-    optionTitles: ["Auto", "Default", "Full"],
-    defaultValue: "auto",
+    title: 'Size',
+    options: ['auto', 'default', 'full'],
+    optionTitles: ['Auto', 'Default', 'Full'],
+    defaultValue: 'auto',
     type: ControlType.Enum,
   },
-}
+};
 
 export function merge(inferredControls: ControlDescription, overrides: {}): ControlDescription {
-  return { ...inferredControls, ...overrides }
+  return {...inferredControls, ...overrides};
 }
